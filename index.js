4e12025b4c3842648ab31c48c5e0293e5718e6c7
Jiu Liancheng <emlcjnil@gmail.com>
2016-12-19 20:43:07 +0800

    add automatic hitokoto response

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const postgraphql = require('postgraphql').postgraphql
 const convert = require('koa-convert')
 const wecaht = require('co-wechat')
 
+const client = require('./lib/client')
+const parseHitokoto = require('./lib/parser')
+
 const pgConfig = process.env.PG_CONFIG || 'postgres://localhost:5432'
 const wechatToken = process.env.WECHAT_TOKEN || 'place_holder_for_wecaht_token'
 const wechatAppId = process.env.WECHAT_APPID || 'placeholder_for_appid'
-const aesKey = process.env.AES_KEY || 'aes_key'
+const aesKey = process.env.AES_KEY || 'NVddLuYYwb6cflxF6JAR1AYiwR6Z6Rxp2dsjsSM8xr3'
 const isDebug = process.env.NODE_ENV === 'development'
 
 const app = new Koa()
@@ -26,9 +29,8 @@ app.use(convert(wecaht({
   token: wechatToken,
   encodingAESKey: aesKey
 }).middleware(function *() {
-  const message = this.weixin
-  console.log(message)
-  this.body = 'test automatic response'
+  const res = yield client.getRandomHitokoto()
+  this.body = parseHitokoto(res.data.randomHitokoto)
 })))
 
 app.use((ctx) => {
